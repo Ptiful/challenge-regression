3a360668cc8084a24642382cd909ea4bc0fade1a
Gregoire Hupin <hupingregoire@hotmail.com>
2023-05-31 15:05:01 +0200

    Code in full_scrape_list

--- src/full_scrape_list.py
+++ src/full_scrape_list.py
@@ -3,10 +3,9 @@ end_url = "&orderBy=relevance"
 
 list_of_search_pages = []
 step = 25000
-for i in range(20,40):
-    print(i)
-"""
-for index in range(40):
+
+
+for index in range((1e6/step)+1):
     min_price = index*step
     max_price = min_price +step - 1
     base_url = f"https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&priceType=SALE_PRICE&minPrice={min_price}&maxPrice={max_price}&page="
@@ -14,10 +13,19 @@ for index in range(40):
         list_of_search_pages.append(base_url+str(page_number)+end_url)
 
 step = 50000
-for index in range(20, 40):
+for index in range(1e6/step, 3e6/step+1):
     min_price = index*step
     max_price = min_price +step - 1
     base_url = f"https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&priceType=SALE_PRICE&minPrice={min_price}&maxPrice={max_price}&page="
     for page_number in range(25):
         list_of_search_pages.append(base_url+str(page_number)+end_url)
-        """
+
+step = 500000
+for index in range(3e6/step, 15e6/step+1):
+    min_price = index*step
+    max_price = min_price +step - 1
+    base_url = f"https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&priceType=SALE_PRICE&minPrice={min_price}&maxPrice={max_price}&page="
+    for page_number in range(25):
+        list_of_search_pages.append(base_url+str(page_number)+end_url)
+
+
